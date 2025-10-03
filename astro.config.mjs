443ae5e4c4e7043748112171c56ea1c0a7519e5e
Kenyo <kenyoramirez110504@gmail.com>
2025-10-02 21:40:39 -0500

    fix: add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://kenyo1924.github.io',
   base: '/mi-portafolio',
+  integrations: [icon()], 
 });
